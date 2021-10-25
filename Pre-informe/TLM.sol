07086ee5f9b9216d6b82a1ccb9e6ac4e11f851fb
rcarrion14 <87649418+rcarrion14@users.noreply.github.com>
2021-10-25 17:23:23 -0300

    Add files via upload

---
+++ Pre-informe/TLM.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "/contracts/TLMbase.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "/contracts/Vaulteable.sol";
+
+contract TLM is Ownable, Vaulteable, TLMbase {
+    
+    bool minter = true;
+    
+    constructor (string memory name_, string memory symbol_ ) TLMbase(name_, symbol_) {}
+    
+    
+    function mint(address account, uint256 amount) external onlyVault {
+        require (minter == true, "TLM: TLM already minted" );
+        _mint(account, amount);
+        minter = false ;
+        
+    }
+    
+    function burn(address account, uint256 amount) external virtual onlyVault{
+        _burn(account, amount);
+    }
+
+}
